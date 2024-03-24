4a89b2fac1c6c7be074dc74b891c54cfe6327752
facundo enriquez <facundo.tomas.enriquez@gmail.com>
2024-03-24 20:14:56 -0300

    checkin

--- comun_grafo.py
+++ comun_grafo.py
@@ -7,9 +7,6 @@ G = nx.Graph()
 # distancias en linea recta al nodo "B"
 dlr = {"A": 77, "B": 00, "X": 55, "F": 12, "P": 10, "S": 22}
 
-for node, value in dlr.items():
-    print(node, value)
-
 # Añadir nodos
 nodos = ['A', 'X', 'P', 'F', 'S', 'B']
 G.add_nodes_from(nodos)
@@ -28,39 +25,51 @@ G.add_edges_from(uniones)
 nodo_inicial = 'A'  # Cambiar a cualquier nodo deseado
 nodo_final = 'B'  # Cambiar a cualquier nodo deseado
 
-# Calcular el camino más corto
-shortest_path = nx.shortest_path(G, source=nodo_inicial, target=nodo_final)
+if nodo_inicial not in nodos or nodo_final not in nodos:
+    print(f"El nodo inicial {nodo_inicial} o nodo final {
+          nodo_final} no se encuentran en la lista de nodos")
 
-# Dibujar el grafo
-posiciones_nodos = nx.spring_layout(G)  # Posiciones de los nodos
+uniones_locales = []
+# Iterar sobre las uniones
+for union in uniones:
+    if nodo_inicial == union[0] or nodo_inicial == union[1]:  # Verificar si el nodo inicial está en la unión
+        uniones_locales.append(union)
+        
+print(uniones_locales)
 
-print(G.nodes())
+# # Calcular el camino más corto
+# shortest_path = nx.shortest_path(G, source=nodo_inicial, target=nodo_final)
 
-nx.draw(G, posiciones_nodos, with_labels=True, node_size=900,
-        node_color="skyblue", labels={node: node for node in G.nodes()})
-edge_labels_props = nx.draw_networkx_edge_labels(G, posiciones_nodos, edge_labels={
-                                                 (u, v): d['weight'] for u, v, d in G.edges(data=True)})
+# # Dibujar el grafo
+# posiciones_nodos = nx.spring_layout(G)  # Posiciones de los nodos
 
-print(shortest_path)
+# print(G.nodes())
 
-if nodo_inicial:
-    nx.draw_networkx_nodes(G, posiciones_nodos, nodelist=[
-                           nodo_inicial], node_color='red', node_size=900)
-if nodo_final:
-    nx.draw_networkx_nodes(G, posiciones_nodos, nodelist=[
-                           nodo_final], node_color='green', node_size=900)
+# nx.draw(G, posiciones_nodos, with_labels=True, node_size=900,
+#         node_color="skyblue", labels={node: node for node in G.nodes()})
+# edge_labels_props = nx.draw_networkx_edge_labels(G, posiciones_nodos, edge_labels={
+#                                                  (u, v): d['weight'] for u, v, d in G.edges(data=True)})
 
-# Dibujar los bordes del grafo sin el camino más corto
-nx.draw_networkx_edges(G, posiciones_nodos,
-                       edgelist=G.edges(), width=1.0, alpha=0.5)
+# print(shortest_path)
 
-# Dibujar el camino más corto
-shortest_path_edges = [(shortest_path[i], shortest_path[i+1])
-                       for i in range(len(shortest_path)-1)]
-nx.draw_networkx_edges(G, posiciones_nodos,
-                       edgelist=shortest_path_edges, width=2, edge_color='red')
+# if nodo_inicial:
+#     nx.draw_networkx_nodes(G, posiciones_nodos, nodelist=[
+#                            nodo_inicial], node_color='red', node_size=900)
+# if nodo_final:
+#     nx.draw_networkx_nodes(G, posiciones_nodos, nodelist=[
+#                            nodo_final], node_color='green', node_size=900)
 
-# Dibujar las etiquetas de los bordes (pesos)
-edge_labels = nx.draw_networkx_edge_labels(G, posiciones_nodos, edge_labels={(u, v): d['weight']
-                                                                             for u, v, d in G.edges(data=True)})
-plt.show()
+# # Dibujar los bordes del grafo sin el camino más corto
+# nx.draw_networkx_edges(G, posiciones_nodos,
+#                        edgelist=G.edges(), width=1.0, alpha=0.5)
+
+# # Dibujar el camino más corto
+# shortest_path_edges = [(shortest_path[i], shortest_path[i+1])
+#                        for i in range(len(shortest_path)-1)]
+# nx.draw_networkx_edges(G, posiciones_nodos,
+#                        edgelist=shortest_path_edges, width=2, edge_color='red')
+
+# # Dibujar las etiquetas de los bordes (pesos)
+# edge_labels = nx.draw_networkx_edge_labels(G, posiciones_nodos, edge_labels={(u, v): d['weight']
+#                                                                              for u, v, d in G.edges(data=True)})
+# plt.show()
