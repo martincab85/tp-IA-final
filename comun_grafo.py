17593fda05b31d849985a2ed089f45ee81a75cbe
Facundo Enriquez <fenriquez@inym.org.ar>
2024-03-27 13:55:23 -0300

    roling back to grafs

--- comun_grafo.py
+++ comun_grafo.py
@@ -1,46 +1,35 @@
 import networkx as nx
 import matplotlib.pyplot as plt
-import tkinter as tk
+from matplotlib.widgets import Button
+from datos_grafo import dlr_20_1, nodos_20_1, uniones_20_1, nodo_incial_20_1, nodo_final_20_1, dlr_20_2, nodos_20_2, uniones_20_2, nodo_incial_20_2, nodo_final_20_2, dlr_20_5, nodos_20_5, uniones_20_5, nodo_incial_20_5, nodo_final_20_5
+
+# Seleccionar el número de grafo que deseas utilizar
+numero_de_grafo = 20.5  # Cambia a 20.1 si deseas utilizar el primer grafo
+
+# Acceder a los datos correspondientes al número de grafo seleccionado
+if numero_de_grafo == 20.1:
+    nodo_inicial = nodo_incial_20_1
+    nodo_final = nodo_incial_20_1
+    dlr = dlr_20_1
+    nodos = nodos_20_1
+    uniones = uniones_20_1
+elif numero_de_grafo == 20.2:
+    nodo_inicial = nodo_incial_20_2
+    nodo_final = nodo_final_20_2
+    dlr = dlr_20_2
+    nodos = nodos_20_2
+    uniones = uniones_20_2
+elif numero_de_grafo == 20.5:
+    nodo_inicial = nodo_incial_20_5
+    nodo_final = nodo_final_20_5
+    dlr = dlr_20_5
+    nodos = nodos_20_5
+    uniones = uniones_20_5
 
 # Crear un grafo
 G = nx.Graph()
-
-# distancias en linea recta al nodo "B"
-# grafo 20.1
-dlr = {"A": 60, "B": 40, "C": 50, "D": 10, "E": 20, "F": 00}
-# grafo 20.2
-# dlr = {"A": 77, "B": 00, "X": 55, "F": 12, "P": 10, "S": 22}
-
-# Añadir nodos
-# grafo 20.1
-nodos = ['A', 'B', 'C', 'D', 'E', 'F']
-# grafo 20.2
-# nodos = ['A', 'X', 'P', 'F', 'S', 'B']
 G.add_nodes_from(nodos)
-
-# Añadir uniones con pesos
-# grafo 20.1
-uniones = [
-    ('A', 'B'), ('A', 'E'), ('A', 'D'),
-    ('B', 'C'), ('B', 'E'), ('B', 'D'), ('B', 'F'),
-    ('C', 'D'), ('C', 'F'),
-    ('D', 'E'),
-]
-# grafo 20.2
-# uniones = [
-#     ('A', 'X'), ('A', 'P'), ('A', 'F'),
-#     ('X', 'S'), ('X', 'B'), ('P', 'F'),
-#     ('S', 'F'), ('P', 'S'), ('S', 'B'),
-# ]
 G.add_edges_from(uniones)
-
-# Nodo inicial previamente seleccionado
-nodo_inicial = 'A'  # Cambiar a cualquier nodo deseado
-nodo_final = 'F'  # Cambiar a cualquier nodo deseado
-
-if nodo_inicial not in nodos or nodo_final not in nodos:
-    print(f"El nodo inicial {nodo_inicial} o nodo final {
-          nodo_final} no se encuentran en la lista de nodos")
 
 nodos_actuales = {nodo_inicial: True}  # A
 nodos_sucesores = []
@@ -48,7 +37,6 @@ nodos_sucesores = []
 
 def get_uniones(nodo):
     uniones_locales = []
-    nodo_actual_value = dlr[nodo]
     # Iterar sobre las uniones
     for union in uniones:
         # Verificar si el nodo inicial está en la unión
@@ -71,87 +59,34 @@ def check_nodo_siguiente():
     nodo_actual = get_actual_node()
     uniones = get_uniones(nodo_actual)
 
-    while uniones:
-        nodo_siguiente = min(uniones) if uniones else None
-        nodos_sucesores.append(nodo_siguiente)
+    if nodo_actual != nodo_final:
+        while uniones:
+            nodo_siguiente = min(uniones) if uniones else None
+            if nodo_siguiente not in nodos_sucesores:
+                nodos_sucesores.append(nodo_siguiente)
 
-        nodo_actual_value = dlr[nodo_actual]
-        nodo_siguiente_value = dlr[nodo_siguiente]
+            nodo_actual_value = dlr[nodo_actual]
+            nodo_siguiente_value = dlr[nodo_siguiente]
 
-        print(nodo_actual, ' --- nodo actual')
-        print(uniones, ' --- uniones')
-        print(nodo_siguiente, ' --- nodo siguiente')
-        print(nodos_sucesores, ' --- nodos succesores')
+            print(nodo_actual, ' --- nodo actual')
+            print(uniones, ' --- uniones')
+            print(nodo_siguiente, ' --- nodo siguiente')
+            print(nodos_sucesores, ' --- nodos succesores')
 
-        if nodo_siguiente_value <= nodo_actual_value:
-            nodos_actuales[nodo_actual] = False
-            nodos_actuales[nodo_siguiente] = True
-            print(nodos_actuales, ' --- nodos actuales')
-            print('-----------------------------------------------------')
-            print('\n')
-            break
-        else:
-            uniones.remove(nodo_siguiente)
+            if nodo_siguiente_value <= nodo_actual_value:
+                nodos_actuales[nodo_actual] = False
+                nodos_actuales[nodo_siguiente] = True
+                print(nodos_actuales, ' --- nodos actuales')
+                print('-----------------------------------------------------')
+                print('\n')
+                break
+            else:
+                uniones.remove(nodo_siguiente)
 
-        label.config(text=f"Nodo actual: {nodo_siguiente}")
     else:
+        print('\n')
         print('se termino el programa')
         print(nodos_actuales)
         print(nodos_sucesores)
 
-
-# Crear ventana
-window = tk.Tk()
-window.title("Interfaz para iterar")
-
-# Crear etiqueta para mostrar el nodo actual
-label = tk.Label(window, text="Nodo actual:")
-label.pack()
-
-# Crear botón para llamar a la función
-button = tk.Button(window, text="Siguiente Iteración",
-                   command=check_nodo_siguiente)
-button.pack()
-
-# Iniciar bucle de eventos
-window.mainloop()
-
-# check_nodo_siguiente()
-
-
-# # Calcular el camino más corto
-# shortest_path = nx.shortest_path(G, source=nodo_inicial, target=nodo_final)
-
-# # Dibujar el grafo
-# posiciones_nodos = nx.spring_layout(G)  # Posiciones de los nodos
-
-# print(G.nodes())
-
-# nx.draw(G, posiciones_nodos, with_labels=True, node_size=900,
-#         node_color="skyblue", labels={node: node for node in G.nodes()})
-# edge_labels_props = nx.draw_networkx_edge_labels(G, posiciones_nodos, edge_labels={
-#                                                  (u, v): d['weight'] for u, v, d in G.edges(data=True)})
-
-# print(shortest_path)
-
-# if nodo_inicial:
-#     nx.draw_networkx_nodes(G, posiciones_nodos, nodelist=[
-#                            nodo_inicial], node_color='red', node_size=900)
-# if nodo_final:
-#     nx.draw_networkx_nodes(G, posiciones_nodos, nodelist=[
-#                            nodo_final], node_color='green', node_size=900)
-
-# # Dibujar los bordes del grafo sin el camino más corto
-# nx.draw_networkx_edges(G, posiciones_nodos,
-#                        edgelist=G.edges(), width=1.0, alpha=0.5)
-
-# # Dibujar el camino más corto
-# shortest_path_edges = [(shortest_path[i], shortest_path[i+1])
-#                        for i in range(len(shortest_path)-1)]
-# nx.draw_networkx_edges(G, posiciones_nodos,
-#                        edgelist=shortest_path_edges, width=2, edge_color='red')
-
-# # Dibujar las etiquetas de los bordes (pesos)
-# edge_labels = nx.draw_networkx_edge_labels(G, posiciones_nodos, edge_labels={(u, v): d['weight']
-#                                                                              for u, v, d in G.edges(data=True)})
-# plt.show()
+check_nodo_siguiente()
